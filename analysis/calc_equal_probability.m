40f732b3abe31ce73abce0870987e87cca2da2d9
Hythem Sidky <hsidky@nd.edu>
2015-02-22 21:03:05 -0500

    Improved equal probability calc.

--- analysis/calc_equal_probability.m
+++ analysis/calc_equal_probability.m
@@ -1,25 +1,51 @@
-function p = calc_equal_probability(f, x1, x2)
+function bopt = calc_equal_probability(fxn, xrng, brng)
+% CALC_EQUAL_PROBABILITY - Applies the equal area rule to calculate phase
+% equilibrium between two states. Expects the parameter bounds to exhibit 
+% bimodal probability distribution. The routine identifies the two most 
+% prominent peaks and the minimum (valley) between them and integrates
+% between the bounds and the valley.
+% 
+% Inputs: 
+% fxn  - Function handle f(x,b) to a probability distribution where x is the
+%        independent variable and b is an adjustable parameter. 
+% xrng - Independent parameter range over which to integrate fnx [xmin, xmax].
+% brng - Bounds on the value of independent parameter [bmin, bmax].
+% 
+% Outputs:
+% bopt - The adjustable parameter, bopt, at which the equal area rule is
+%        satisfied.
 
-fobj = @(x)objective_function(f,x);
+fobj = @(b) objective_function(fxn,b,xrng);
+bopt = fminbnd(fobj, brng(1), brng(2));
 
-p = fminbnd(fobj, x1, x2, optimset('display', 'iter'));
+function r = objective_function(f, b, xrng)
+x = linspace(xrng(1),xrng(2),1000)';
+y = f(x,b);
 
-function r = objective_function(f,b)
-y = f(b);
-[pks, locs]= findpeaks(y, 'MinPeakWidth', 10, 'MinPeakDistance', 100, 'SortStr', 'desc');
+mindx = 0.01*diff(xrng);
+minxw = 0.01*diff(xrng);
 
-if length(pks) == 1
+% Get peaks
+[~, xpks]= findpeaks(y, x, 'MinPeakWidth', minxw, 'MinPeakDistance', mindx, 'SortStr', 'desc');
+
+if length(xpks) ~= 2
     error('peak_identify::There must be two identifiable peaks at the starting guess.');
 end
 
-% Find minimum between peaks 
-[~, mloc] = min(y(min(locs):max(locs)));
+[~,lloc] = min(abs(min(xpks)-x));
+[~,uloc] = min(abs(max(xpks)-x));
 
-mloc = mloc + min(locs);
+% Find minimum between peaks 
+[~, mloc] = min(y(lloc:uloc));
+mloc = mloc + lloc;
 
 % Integrate between peaks until ends 
-i1 = trapz(y(min(locs):mloc));
-i2 = trapz(y(mloc:max(locs)));
+i1 = integral(@(x)f(x,b), x(1), x(mloc));
+i2 = integral(@(x)f(x,b), x(mloc), x(end));
 
+fprintf('\n');
+fprintf('  Identified peaks: %.5f       %.5f \n',x(lloc),x(uloc));
+fprintf('  Peak integrals:   %.5f       %.5f \n', i1, i2);
+fprintf('  Minimum between peaks:   %.5f \n', x(mloc));
 r = abs(i1 - i2);
     
