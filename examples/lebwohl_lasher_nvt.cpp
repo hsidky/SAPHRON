5d0a3d62c68da3cdfc4efaf14377f04d8f97a481
Hythem Sidky <sidky@outlook.com>
2015-04-18 10:59:22 -0400

    Update LL NVT example

--- examples/lebwohl_lasher_nvt.cpp
+++ examples/lebwohl_lasher_nvt.cpp
@@ -4,64 +4,101 @@
 
 // Include the pertinent header files.
 #include "../src/Ensembles/NVTEnsemble.h"
-#include "../src/Models/LebwohlLasherModel.h"
+#include "../src/ForceFields/ForceFieldManager.h"
+#include "../src/ForceFields/LebwohlLasherFF.h"
+#include "../src/Moves/MoveManager.h"
 #include "../src/Moves/SphereUnitVectorMove.h"
+#include "../src/Particles/Site.h"
+#include "../src/Simulation/CSVObserver.h"
+#include "../src/Simulation/ConsoleObserver.h"
+#include "../src/Worlds/SimpleLatticeWorld.h"
 
 // Includes for parsing using stringstream.
 #include <iostream>
 #include <sstream>
+
+using namespace SAPHRON;
 
 // Function definition of our basic input parser.
 // // A very basic input parser.
 int parse_input(char const* args[], int& latticeSize,
                 int& iterations,
                 double& temperature,
-                std::string& modelFile,
-                std::string& SitesFile);
+                std::string& filePrefix);
 
 // The main program expects a user to input the lattice size, reduced temperature,
-// number of iterations and specify an output file (csv) for model properties and
-// site properties.
+// number of iterations and specify an output file prefix (csv) for logged properties.
 int main(int argc, char const* argv[])
 {
 	int latticeSize, iterations;
 	double temperature;
-	std::string modelFile, sitesFile;
+	std::string filePrefix;
 
 	// We expect a certain number of arugments.
-	if(argc != 6)
+	if(argc != 5)
 	{
 		std::cerr << "Program syntax:" << argv[0] <<
-		" lattice-size temperature iterations model-outputfile site-outputfile" <<
+		" lattice-size temperature iterations outputfile-prefix" <<
 		std::endl;
 		return 0;
 	}
 
 	// Exit on parsing error.
-	if(parse_input(argv, latticeSize, iterations, temperature, modelFile, sitesFile) != 0)
+	if(parse_input(argv, latticeSize, iterations, temperature, filePrefix) != 0)
 		return -1;
 
-	// Initialize the Lebwohl-Lasher lattice model. The constructor takes in one
-	// required argument which is the lattice size - the length of one dimension.
-	// The model initializes the required number of sites in the appropriate locations,
-	// defines the nearest-neighbors and provides an initial spin.
-	Models::LebwohlLasherModel model(latticeSize, latticeSize, latticeSize);
+	// Initialize a simple cubic lattice "world". This defines the type of box for our simulation.
+	// The constructor takes in the lattice dimensions and an optional seed for the pseudo random 
+	// number generator.
+	SimpleLatticeWorld world(latticeSize, latticeSize, latticeSize, 1);
 
-	// Initialize the NVT ensemble. For this particular ensemble, it requires a reference to
-	// the model and the temperature. The template parametr for the ensemble is the type
-	// returned by the DrawSample method of the model - in this case it is a site.
-	//Ensembles::NVTEnsemble<Site> ensemble(model, temperature);
+	// Initialize a site. This will represent an individual spin on the lattice in our world. 
+	// The constructor takes in an initial position, director and species type (string). Note that 
+	// the position is irrelevant since this site will be copied and configured appropriately for 
+	// the system.
+	Site site1({ 0, 0, 0 }, { 1.0, 0, 0 }, "E1");
 
-	// Initialize the moves. These are the different Monte-Carlo moves we would like
-	// to perform on the model above. In this case, we want to pick a random unit
-	// vector on a sphere. A "Move" class operates on a site within model in an ensemble.
-	Moves::SphereUnitVectorMove move;
+	// Now we configure the world but providing a reference to our spin and telling it to fill the 
+	// box entirely with that site. We then configure connectivity through neighbor list 
+	// initialization.
+	world.ConfigureParticles({ &site1 }, { 1.0 });
+	world.ConfigureNeighborList();
 
-	//ensemble.AddMove(move);
 
-	// Sweep for iterations
-	//for (int i = 0; i < iterations; i++)
-		//ensemble.Sweep();
+	// Initialize the Lebwohl-Lasher (LL) force field and add it to an instance of the forcefield
+	// manager. The LL forcefield takes in two arguments. An anisotropic interaction energy and 
+	// an isotropic term as well. See header file for details.
+	LebwohlLasherFF ff(1.0, 0);
+	ForceFieldManager ffm;
+	ffm.AddForceField("E1", "E1", ff);
+
+	// Initialize the move we want to perform on our sites, then add it to an instance of the 
+	// move manager. The only argument the constructor takes is a seed for the PRNG.
+	SphereUnitVectorMove move1(33);
+	MoveManager mm;
+	mm.PushMove(move1);
+
+	// Initialize the "observers". These are effectively the loggers that will be monitoring the 
+	// simulation. We want to print out some information to the console to monitor the run, 
+	// but store more details in a CSV file format.
+	SimFlags flags1;
+	flags1.temperature = 1;
+	flags1.iterations = 1;
+	flags1.energy = 1;
+	ConsoleObserver console(flags1, 1000);
+
+	SimFlags flags2;
+	flags2.temperature = 1;
+	flags2.iterations = 1;
+	flags2.energy = 1;
+	CSVObserver csv(filePrefix, flags2, 500);
+
+	// Initialize the NVT ensemble. For this particular ensemble, it requires a reference to world, 
+	// the forcefield manager and the move manager. It also takes in a temperature and RNG seed.
+	NVTEnsemble ensemble(world, ffm, mm, temperature, 45);
+
+	// Run the simulation. 
+	ensemble.Run(iterations);
 
 	return 0;
 }
@@ -70,8 +107,7 @@ int main(int argc, char const* argv[])
 int parse_input(char const* args[], int& latticeSize,
                 int& iterations,
                 double& temperature,
-                std::string& modelFile,
-                std::string& SitesFile)
+                std::string& filePrefix)
 {
 	std::stringstream ss;
 
@@ -102,17 +138,9 @@ int parse_input(char const* args[], int& latticeSize,
 
 	ss.clear();
 	ss.str(args[4]);
-	if(!(ss >> modelFile))
+	if(!(ss >> filePrefix))
 	{
-		std::cerr << "Invalid output file. Must be a string." << std::endl;
-		return -1;
-	}
-
-	ss.clear();
-	ss.str(args[5]);
-	if(!(ss >> SitesFile))
-	{
-		std::cerr << "Invalid output file. Must be a string." << std::endl;
+		std::cerr << "Invalid output file prefix. Must be a string." << std::endl;
 		return -1;
 	}
 
