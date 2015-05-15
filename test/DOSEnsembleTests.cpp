f97eb9d52661230ed76153a48192902e7196163c
Hythem Sidky <hsidky@nd.edu>
2015-05-15 15:08:31 -0400

    Minor CMake update.

--- test/DOSEnsembleTests.cpp
+++ test/DOSEnsembleTests.cpp
@@ -6,6 +6,7 @@
 #include "../src/Moves/SphereUnitVectorMove.h"
 #include "../src/Particles/Site.h"
 #include "../src/Simulation/CSVObserver.h"
+#include "../src/Simulation/ConsoleObserver.h"
 #include "../src/Worlds/SimpleLatticeWorld.h"
 #include "gtest/gtest.h"
 
@@ -14,7 +15,7 @@ using namespace SAPHRON;
 
 TEST(DOSEnsemble, DefaultBehavior)
 {
-	int n = 25;
+	int n = 20;
 	// Initialize world.
 	SimpleLatticeWorld world(n, n, n, 1);
 	Site site1({0, 0, 0}, {1.0, 0, 0}, "E1");
@@ -42,12 +43,20 @@ TEST(DOSEnsemble, DefaultBehavior)
 	flags.dos_bin_count = 1;
 	flags.dos_scale_factor = 1;
 	flags.dos_values = 1;
-	CSVObserver co("test", flags, 100);
+	CSVObserver co("test", flags, 10000);
+	SimFlags flags2;
+	flags2.iterations = 1;
+	flags2.energy = 1;
+	flags2.dos_flatness = 1;
+	flags2.dos_interval = 1;
+	flags2.dos_scale_factor = 1;
+	ConsoleObserver co2(flags2, 5000);
 
 	// Initialize Wang-Landau order parameter. 
 	WangLandauOP op;
 
-	DOSEnsemble ensemble(op, world, ffm, mm, {-1.7*n*n*n, -0.5*n*n*n}, 5000, 1);
+	DOSEnsemble ensemble(op, world, ffm, mm, {-1.8*n*n*n, 0.3*n*n*n}, 7000, 1);
 	ensemble.AddObserver(&co);
-	ensemble.Run(1);
+	ensemble.AddObserver(&co2);
+	ensemble.Run(20);
 }
