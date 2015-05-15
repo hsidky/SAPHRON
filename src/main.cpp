8ed2bf32d943f8947ab7df9cbb5c34fd8d543374
Hythem Sidky <hsidky@nd.edu>
2015-05-15 18:20:26 -0400

    Initial configparser addition.

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,31 @@
 // Include for parsing using stringstream
 #include <iostream>
 #include <sstream>
+#include <locale.h>
+#include "config4cpp/Configuration.h"
+
+using namespace config4cpp;
 
 // The main program expects a user to input the lattice size, number of EXEDOS
 // iterations, minimum and maximum mole fractions, number of bins for density-of-states
 // histogram and model, sites and vector file outputs.
 int main(int argc, char const* argv[])
 {
+	const char *    logDir;
+	int             logLevel, timeout;
+	const char *    scope = "foo_srv";
+	Configuration * cfg = Configuration::create();
+	
+	try
+	{
+		cfg->parse("foo.cfg");
+	  	logDir   = cfg->lookupString(scope, "log.dir");
+	  	logLevel = cfg->lookupInt(scope, "log.level");
+	  	timeout  = cfg->lookupDurationSeconds(scope, "timeout");
+	} 
+	catch(const ConfigurationException & ex) {
+		std::cout << ex.c_str() << std::endl;
+	}
+	cfg->destroy();
 	return 0;
 }
