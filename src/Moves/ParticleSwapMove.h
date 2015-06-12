be49fd437fa71141eeed4eb7f3c20b2d5f195b60
Hythem Sidky <hsidky@nd.edu>
2015-06-12 18:12:49 -0400

    Particle swap skeleton.

---
+++ src/Moves/ParticleSwapMove.h
@@ -0,0 +1,14 @@
+#pragma once 
+
+#include "GibbsMove.h"
+
+namespace SAPHRON
+{
+	// Class for performing a particle swap between two worlds.
+	// Note: this does not implement the Move interface (leaves them empty). 
+	// It only implements the GibbsMove interface.
+	class ParticleSwapMove : public GibbsMove
+	{
+		
+	};
+}
