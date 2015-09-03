f7be08b6420340c49d7792e8c579e5e1fd6ff256
Hythem Sidky <hsidky@nd.edu>
2015-09-03 17:09:22 -0400

    Added sim info singleton.

---
+++ src/Simulation/SimInfo.h
@@ -0,0 +1,23 @@
+namespace SAPHRON
+{
+	// Class that hold useful information to be used across SAPHRON
+	// such as physical constants. 
+	class SimInfo
+	{
+	private:
+		double _kb = 1.0;
+	public:
+
+		// Get singleton (I know, I know...) instance of 
+		// SimInfo.
+		static SimInfo& Instance() 
+		{
+    		static SimInfo instance;
+    		return instance;
+		}
+
+		// Get/set Boltzmann constant.
+		double GetkB() { return _kb; }
+		void SetkB(double kb) { _kb = kb; }
+	};
+}
