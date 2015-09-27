6cb515aac3842fee8a58beb7f3b1060352360f9d
Hythem Sidky <sidky@outlook.com>
2015-09-27 12:13:14 -0400

    Added few more physical constants.

--- src/Simulation/SimInfo.h
+++ src/Simulation/SimInfo.h
@@ -7,7 +7,17 @@ namespace SAPHRON
 	class SimInfo
 	{
 	private:
+		// Boltzmann constant.
 		double _kb = 1.0;
+
+		// Boltzmann constant (J/K).
+		double _KB = 1.3806488E-23;
+
+		// Permittivity of vacuum C2/(J angstrom).
+		double _vacperm = 8.854187817E-22;
+
+		// Elementary charge (C).
+		double _elem = 1.602176565E-19;
 	public:
 
 		// Get singleton (I know, I know...) instance of 
@@ -18,8 +28,19 @@ namespace SAPHRON
     		return instance;
 		}
 
+		// Get Boltzmann constant in default units (J/K).
+		double GetkBDefault() const { return _KB; }
+		
 		// Get/set Boltzmann constant.
-		double GetkB() { return _kb; }
+		double GetkB() const { return _kb; }
 		void SetkB(double kb) { _kb = kb; }
+
+		// Get/set vacuum permittivity.
+		double GetVacPerm() const { return _vacperm; }
+		void SetVacPerm(double eps) { _vacperm = eps; }
+
+		// Get/set elementary charge.
+		double GetElemCharge() const { return _elem; }
+		void SetElemeCharge(double e) { _elem = e; }
 	};
 }
