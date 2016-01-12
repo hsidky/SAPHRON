c1433c68fe84cac24055d0a577cab88fb3168851
Ben S <bsikora906@gmail.com>
2016-01-12 11:01:25 -0500

    Added the acid reaction move

--- src/Moves/AcidReactionMove.h
+++ src/Moves/AcidReactionMove.h
@@ -1,10 +1,10 @@
 #pragma once 
+
 #include "Move.h"
-#include "../Particles/Particle.h"
-#include "../Worlds/WorldManager.h"
-#include "../Worlds/World.h"
-#include "../ForceFields/ForceFieldManager.h"
+#include "../Utils/Helpers.h"
 #include "../Utils/Rand.h"
+#include "../Worlds/WorldManager.h"
+#include "../ForceFields/ForceFieldManager.h"
 #include "../DensityOfStates/DOSOrderParameter.h"
 
 /* Currently move only works with acid where the following is the reaction:
@@ -14,7 +14,7 @@ Reverse A- + H+ -> HA
 
 namespace SAPHRON
 {
-	class ReactionMove : public Move
+	class AcidReactionMove : public Move
 	{
 	private:
 		std::vector<int> _reactants;
@@ -56,6 +56,7 @@ namespace SAPHRON
 	public:
 		AcidReactionMove(const std::vector<std::string>& reactants,
 		const std::vector<std::string>& products,
+		const WorldManager& wm,
 		int stashcount, double pKo, double protoncharge,
 		int seed = 7456253) : 
 		_reactants(0),_products(0), _rand(seed), _performed(0),
@@ -97,6 +98,7 @@ namespace SAPHRON
 
 		AcidReactionMove(const std::vector<int>& reactants,
 		const std::vector<int>& products,
+		const WorldManager& wm,
 		int stashcount, double pKo, double protoncharge,
 		int seed = 7456253) : 
 		_reactants(0),_products(0), _rand(seed), _performed(0),
@@ -116,7 +118,7 @@ namespace SAPHRON
 							  << std::endl;
 					exit(-1);
 				}
-				_Reactants.push_back(id);
+				_reactants.push_back(id);
 			}
 			
 			for(auto& id : products)
@@ -141,13 +143,14 @@ namespace SAPHRON
 
 			// Draw random particle from random world.
 			auto* w = wm->GetRandomWorld();
-			auto* p = wm->DrawRandomPrimitiveBySpecies(_reactants[0]);
-			particle* ph =nullptr;
+			auto* p = w->DrawRandomPrimitiveBySpecies(_reactants[0]);
+			Particle* ph =nullptr;
 
 			int RxnExtent = 1;
 			double rxndirection = _rand.doub();
 			double OldCharge = p->GetCharge();
 			double OldMass = p->GetMass();
+			double Korxn = 0;
 
 			//Determine reaction direction.
 			if(rxndirection>=0.5 && OldCharge >= 0) //Forward reaction
@@ -158,7 +161,7 @@ namespace SAPHRON
 			else if(rxndirection<0.5 && OldCharge < 0)//Reverse reaction
 			{
 				RxnExtent = -1;
-				ph = wm->DrawRandomPrimitiveBySpecies(_products[0])
+				ph = w->DrawRandomPrimitiveBySpecies(_products[0]);
 
 				if(ph==nullptr)
 				{
@@ -171,7 +174,7 @@ namespace SAPHRON
 				return;
 
 			// Evaluate initial energy. 
-			auto ei = ffm->EvaluateHamiltonian(w);
+			auto ei = ffm->EvaluateHamiltonian(*w);
 
 			if(RxnExtent == -1)
 			{
@@ -216,22 +219,21 @@ namespace SAPHRON
 			auto lamda3 = pow(lambda*lambda*lambda,-1*RxnExtent);
 			double lamdaratio = pow(p->GetMass()/OldMass,3.0/2.0);
 			double Nratio=0;
-			double Kaenergy=0;
 
 			if(RxnExtent==1)
 			{
-				Nratio = (double)(comp[p->GetSpeciesID]-_Ndeprotonated)/((_Ndeprotonated+1)(comp[ph->GetSpeciesID]+1));
-				Kaenergy=inserthere;
+				Nratio = (comp[p->GetSpeciesID()]-_Ndeprotonated)/((_Ndeprotonated+1)*(comp[ph->GetSpeciesID()]+1));
+				Korxn = exp(-_pKo);
 			}
 			else
 			{
-				Nratio = (double)(_Ndeprotonated*comp[ph->GetSpeciesID])/(comp[p->GetSpeciesID]-_Ndeprotonated + 1);
-				Kaenergy=inserthere;
+				Nratio = double(_Ndeprotonated)*comp[ph->GetSpeciesID()]/(comp[p->GetSpeciesID()]-_Ndeprotonated + 1);
+				Korxn = exp(-_pKo);
 			}
 
 			++_performed;
 
-			auto ef = ffm->EvaluateHamiltonian(w);
+			auto ef = ffm->EvaluateHamiltonian(*w);
 
 			auto de = ef - ei;
 		
@@ -239,7 +241,7 @@ namespace SAPHRON
 			auto& sim = SimInfo::Instance();
 
 			// Acceptance probability.
-			double pacc = Nratio*V*lamda3*lamdaratio*Kaenergy*
+			double pacc = Nratio*V*lamda3*lamdaratio*Korxn*
 			exp((-de.energy.total())/(w->GetTemperature()*sim.GetkB()));
 			pacc = pacc > 1.0 ? 1.0 : pacc;
 
@@ -270,64 +272,109 @@ namespace SAPHRON
 			}
 		}
 
-		// Perform move interface for DOS ensemble.
 		virtual void Perform(World* world, 
 							 ForceFieldManager* ffm, 
 							 DOSOrderParameter* op, 
 							 const MoveOverride& override) override
 		{
 
-			auto* p = world->DrawRandomParticle();
+			// Draw random particle from random world.
+			auto* p = world->DrawRandomPrimitiveBySpecies(_reactants[0]);
+			Particle* ph =nullptr;
 
-			if(p == nullptr)
-				return;
+			int RxnExtent = 1;
+			double rxndirection = _rand.doub();
+			double OldCharge = p->GetCharge();
+			double OldMass = p->GetMass();
+			double Korxn = 0;
 
-			if(std::find(_species.begin(), _species.end(), p->GetSpeciesID()) == _species.end())
-				return;
-
-			auto* p1 = p;
-
-			if(p->HasChildren())
+			//Determine reaction direction.
+			if(rxndirection>=0.5 && OldCharge >= 0) //Forward reaction
 			{
-				auto& children = p->GetChildren();
-				auto n = children.size();
-
-				// Draw a random child.
-				auto n1 = _rand.int32() % n;
-
-				// Get child.
-				p1 = children[n1];
+				RxnExtent = 1;
 			}
+
+			else if(rxndirection<0.5 && OldCharge < 0)//Reverse reaction
+			{
+				RxnExtent = -1;
+				ph = world->DrawRandomPrimitiveBySpecies(_products[0]);
+
+				if(ph==nullptr)
+				{
+					std::cout<<"No products are present in system!"<<std::endl;
+					exit(-1);
+				}
+			}
+
+			else
+				return;
 
 			// Evaluate initial energy. 
-			auto ei = ffm->EvaluateHamiltonian(*p1, world->GetComposition(), world->GetVolume());
+			auto ei = ffm->EvaluateHamiltonian(*world);
 			auto opi = op->EvaluateOrderParameter(*world);
 
-			// Perform protonation/deprotonation.
-			auto tc=p1->GetCharge();
-			auto amu = _mu;
-
-			//Deprotonate acid if protonated
-			if(tc>=0.0)
+			if(RxnExtent == -1)
 			{
-				p1->SetCharge(-_protoncharge);
-				amu = -_mu;
+				p->SetCharge(0.0);
+				p->SetMass(OldMass + ph->GetMass());
+				world->RemoveParticle(ph);
 			}
 
-			//Protonate if deprotonated
 			else
 			{
-				p1->SetCharge(0.0);
+				ph = world->UnstashParticle(_products[0]);
+				p->SetCharge(-_protoncharge);
+				p->SetMass(OldMass - ph->GetMass());
+
+				// Generate a random position and orientation for particle insertion.
+				const auto& H = world->GetHMatrix();
+				Vector3D pr{_rand.doub(), _rand.doub(), _rand.doub()};
+				Vector3D pos = H*pr;
+				ph->SetPosition(pos);
+
+				// Choose random axis, and generate random angle.
+				int axis = _rand.int32() % 3 + 1;
+				double deg = (4.0*_rand.doub() - 2.0)*M_PI;
+				Matrix3D R = GenRotationMatrix(axis, deg);
+
+				// Rotate particle and director.
+				ph->SetDirector(R*ph->GetDirector());
+				for(auto& child : *ph)
+				{
+					child->SetPosition(R*(child->GetPosition()-pos) + pos);
+					child->SetDirector(R*child->GetDirector());
+				}
+
+				// Insert particle.
+				world->AddParticle(ph);
+			}
+
+			auto id = ph->GetSpeciesID();
+			auto& comp = world->GetComposition();
+			auto V = pow(world->GetVolume(),RxnExtent);
+			auto lambda = world->GetWavelength(id);
+			auto lamda3 = pow(lambda*lambda*lambda,-1*RxnExtent);
+			double lamdaratio = pow(p->GetMass()/OldMass,3.0/2.0);
+			double Nratio=0;
+
+			if(RxnExtent==1)
+			{
+				Nratio = (double(comp[p->GetSpeciesID()]) - _Ndeprotonated)/((_Ndeprotonated+1)*(comp[ph->GetSpeciesID()]+1));
+				Korxn = exp(-_pKo);
+			}
+			else
+			{
+				Nratio = (double(_Ndeprotonated)*comp[ph->GetSpeciesID()])/(comp[p->GetSpeciesID()]-_Ndeprotonated + 1);
+				Korxn = exp(-_pKo);
 			}
 
 			++_performed;
 
-			auto ef = ffm->EvaluateHamiltonian(*p1, world->GetComposition(), world->GetVolume());
-			auto de = ef - ei;
+			auto ef = ffm->EvaluateHamiltonian(*world);
 
 			// Update energies and pressures.
-			world->IncrementEnergy(de.energy);
-			world->IncrementPressure(de.pressure);
+			world->SetEnergy(ef.energy);
+			world->SetPressure(ef.pressure);
 
 			auto opf = op->EvaluateOrderParameter(*world);
 			
@@ -335,18 +382,34 @@ namespace SAPHRON
 			double pacc = op->AcceptanceProbability(ei.energy, ef.energy, opi, opf, *world);
 			if(_prefac)
 			{
-				auto& sim = SimInfo::Instance();
-				auto beta = 1.0/(world->GetTemperature()*sim.GetkB());
-				auto arg = beta*(amu);
-				pacc *= exp(arg);
+				auto arg = Nratio*V*lamda3*lamdaratio*Korxn;
+				pacc *= arg;
 			}
 			pacc = pacc > 1.0 ? 1.0 : pacc;
 
 			// Reject or accept move.
 			if(!(override == ForceAccept) && (pacc < _rand.doub() || override == ForceReject))
 			{
-				p1->SetCharge(tc);
 				++_rejected;
+				p->SetCharge(OldCharge);
+				p->SetMass(OldMass);
+				world->SetEnergy(ei.energy);
+				world->SetPressure(ei.pressure);
+
+				if(RxnExtent == -1) {world->AddParticle(ph);}
+
+				else {world->StashParticle(ph);}
+
+			}
+			else
+			{
+				if(RxnExtent == -1) 
+				{
+					world->StashParticle(ph);
+					_Ndeprotonated-=1;
+				}
+				else
+					_Ndeprotonated+=1;
 			}
 		}
 
@@ -380,12 +443,12 @@ namespace SAPHRON
 				json["products"].append(s);
 		}
 
-		virtual std::string GetName() const override { return "AcidTitration"; }
+		virtual std::string GetName() const override { return "AcidReaction"; }
 
 		// Clone move.
 		Move* Clone() const override
 		{
-			return new AcidTitrationMove(static_cast<const AcidTitrationMove&>(*this));
+			return new AcidReactionMove(static_cast<const AcidReactionMove&>(*this));
 		}
 
 	};
