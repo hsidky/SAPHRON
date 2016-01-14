7dbf2861552e284ba06ac5cd6320c5a22c744e35
Ben S <bsikora906@gmail.com>
2016-01-14 13:14:52 -0500

    Overhaul of Acid reaction move. Including the schema. Works only for specific systems.

--- src/Moves/AcidReactionMove.h
+++ src/Moves/AcidReactionMove.h
@@ -17,7 +17,7 @@ namespace SAPHRON
 	class AcidReactionMove : public Move
 	{
 	private:
-		std::vector<int> _reactants;
+		std::vector<int> _swap;
 		std::vector<int> _products;
 		Rand _rand;
 
@@ -26,8 +26,16 @@ namespace SAPHRON
 		bool _prefac;
 		int _scount;
 		double _pKo;
-		double _protoncharge;
-		int _Ndeprotonated;
+
+		double _m1;
+		double _m2;
+
+		int _i1;
+		int _i2;
+
+		double _c1;
+		double _c2;
+
 		int _seed;
 
 		void InitStashParticles(const WorldManager& wm)
@@ -35,7 +43,6 @@ namespace SAPHRON
 			// Get particle map, find one of the appropriate species 
 			// and clone.
 			auto& plist = Particle::GetParticleMap();
-
 			for(auto& id : _products)
 			{
 				// Since the species exists, we assume there must be at least 
@@ -47,27 +54,28 @@ namespace SAPHRON
 					}
 				);
 
+				auto* P=pcand->second->Clone();
 				// Stash a characteristic amount of the particles in world.
 				for(auto& world : wm)
-					world->StashParticle(pcand->second, _scount);
+					world->StashParticle(P, _scount);
 			}
+
 		}
 
 	public:
-		AcidReactionMove(const std::vector<std::string>& reactants,
+		AcidReactionMove(const std::vector<std::string>& swap,
 		const std::vector<std::string>& products,
 		const WorldManager& wm,
-		int stashcount, double pKo, double protoncharge,
-		int seed = 7456253) : 
-		_reactants(0),_products(0), _rand(seed), _performed(0),
+		int stashcount, double pKo,int seed = 7456253) : 
+		_swap(0),_products(0), _rand(seed), _performed(0),
 		 _rejected(0), _prefac(true), _scount(stashcount),
-		 _pKo(pKo), _protoncharge(protoncharge),
-		 _Ndeprotonated(0), _seed(seed)
+		 _pKo(pKo), _m1(0), _m2(0), _i1(0), _i2(1), _c1(0),
+		 _c2(0), _seed(seed)
 		{
 			// Verify species list and add to local vector.
 			auto& list = Particle::GetSpeciesList();
 
-			for(auto& id : reactants)
+			for(auto& id : swap)
 			{
 				auto it = std::find(list.begin(), list.end(), id);
 				if(it == list.end())
@@ -77,7 +85,7 @@ namespace SAPHRON
 							  << std::endl;
 					exit(-1);
 				}
-				_reactants.push_back(it - list.begin());
+				_swap.push_back(it - list.begin());
 			}
 
 			for(auto& id : products)
@@ -94,22 +102,48 @@ namespace SAPHRON
 			}
 
 			InitStashParticles(wm);
+
+			auto& plist = Particle::GetParticleMap();
+
+			auto id =_swap[0];
+			auto p1 = std::find_if(plist.begin(), plist.end(), 
+					[=](const std::pair<int, Particle*>& p)
+					{
+						return p.second->GetSpeciesID() == id;
+					}
+				);
+			id =_swap[1];			
+			auto p2 = std::find_if(plist.begin(), plist.end(), 
+					[=](const std::pair<int, Particle*>& p)
+					{
+						return p.second->GetSpeciesID() == id;
+					}
+				);
+			
+			_c1=p1->second->GetCharge();
+			_c2=p2->second->GetCharge();
+
+			_m1=p1->second->GetMass();
+			_m2=p2->second->GetMass();
+
+			_i1 = p1->second->GetSpeciesID();
+			_i2 = p2->second->GetSpeciesID();
+
 		}
 
-		AcidReactionMove(const std::vector<int>& reactants,
+		AcidReactionMove(const std::vector<int>& swap,
 		const std::vector<int>& products,
 		const WorldManager& wm,
-		int stashcount, double pKo, double protoncharge,
-		int seed = 7456253) : 
-		_reactants(0),_products(0), _rand(seed), _performed(0),
+		int stashcount, double pKo,int seed = 7456253) : 
+		_swap(0),_products(0), _rand(seed), _performed(0),
 		 _rejected(0), _prefac(true), _scount(stashcount),
-		 _pKo(pKo), _protoncharge(protoncharge),
-		 _Ndeprotonated(0), _seed(seed)
+		 _pKo(pKo), _m1(0), _m2(0), _i1(0), _i2(1), _c1(0),
+		 _c2(0), _seed(seed)
 		{
 			// Verify species list and add to local vector.
 			auto& list = Particle::GetSpeciesList();
 			
-			for(auto& id : reactants)
+			for(auto& id : swap)
 			{
 				if(id >= (int)list.size())
 				{
@@ -118,7 +152,7 @@ namespace SAPHRON
 							  << std::endl;
 					exit(-1);
 				}
-				_reactants.push_back(id);
+				_swap.push_back(id);
 			}
 			
 			for(auto& id : products)
@@ -134,6 +168,32 @@ namespace SAPHRON
 			}
 
 			InitStashParticles(wm);
+
+			auto& plist = Particle::GetParticleMap();
+
+			auto id =_swap[0];
+			auto p1 = std::find_if(plist.begin(), plist.end(), 
+					[=](const std::pair<int, Particle*>& p)
+					{
+						return p.second->GetSpeciesID() == id;
+					}
+				);
+			id =_swap[1];			
+			auto p2 = std::find_if(plist.begin(), plist.end(), 
+					[=](const std::pair<int, Particle*>& p)
+					{
+						return p.second->GetSpeciesID() == id;
+					}
+				);
+			
+			_c1=p1->second->GetCharge();
+			_c2=p2->second->GetCharge();
+
+			_m1=p1->second->GetMass();
+			_m2=p2->second->GetMass();
+
+			_i1 = p1->second->GetSpeciesID();
+			_i2 = p2->second->GetSpeciesID();
 		} 
 
 		virtual void Perform(WorldManager* wm, 
@@ -143,97 +203,97 @@ namespace SAPHRON
 
 			// Draw random particle from random world.
 			auto* w = wm->GetRandomWorld();
-			auto* p = w->DrawRandomPrimitiveBySpecies(_reactants[0]);
-			Particle* ph =nullptr;
+			Particle* p1 = nullptr;
+			Particle* p2 = nullptr;
+			Particle* ph = nullptr;
 
 			int RxnExtent = 1;
 			double rxndirection = _rand.doub();
-			double OldCharge = p->GetCharge();
-			double OldMass = p->GetMass();
-			double Korxn = 0;
 
 			//Determine reaction direction.
-			if(rxndirection>=0.5 && OldCharge >= 0) //Forward reaction
+			if(rxndirection>=0.5) //Forward reaction
 			{
 				RxnExtent = 1;
+				p1=w->DrawRandomPrimitiveBySpecies(_swap[0]);
+
+				if(p1==nullptr)
+					return;
 			}
 
-			else if(rxndirection<0.5 && OldCharge < 0)//Reverse reaction
+			else //Reverse reaction
 			{
 				RxnExtent = -1;
+				p2 = w->DrawRandomPrimitiveBySpecies(_swap[1]);
 				ph = w->DrawRandomPrimitiveBySpecies(_products[0]);
-
-				if(ph==nullptr)
+	
+				if(ph==nullptr && p2==nullptr)
+					return;
+				
+				else if(ph==nullptr || p2==nullptr)
 				{
-					std::cout<<"No products are present in system!"<<std::endl;
+					std::cout<<"Only Half of the products present! Exiting"<<std::endl;
 					exit(-1);
 				}
 			}
 
-			else
-				return;
+			double lamdaratio;
+
+			auto& comp = w->GetComposition();
+			int comp1 = comp[_i1];
+			int comp2 = comp[_i2];
+			int compph = comp[_products[0]];
+
+			auto V = pow(w->GetVolume(),RxnExtent);
+			auto lambda = w->GetWavelength(_products[0]);
+			auto lamda3 = pow(lambda*lambda*lambda,-1*RxnExtent);
+			double Nratio=0;
+			double Korxn=0;
 
 			// Evaluate initial energy. 
-			auto ei = ffm->EvaluateHamiltonian(*w);
+			EPTuple ei, ef;
 
 			if(RxnExtent == -1)
 			{
-				p->SetCharge(0.0);
-				p->SetMass(OldMass + ph->GetMass());
+				Nratio = comp2*compph/(comp1 + 1.0);
+				Korxn = exp(_pKo);
+				ei = ffm->EvaluateHamiltonian(*ph, w->GetComposition(), w->GetVolume());
 				w->RemoveParticle(ph);
+				ei += ffm->EvaluateHamiltonian(*p2, w->GetComposition(), w->GetVolume());
+				p2->SetCharge(_c1);
+				p2->SetMass(_m1);
+				p2->SetSpeciesID(_i1);
+
+				ef = ffm->EvaluateHamiltonian(*p2, w->GetComposition(), w->GetVolume());
+				lamdaratio = pow(_c1/_c2,3.0/2.0);
 			}
 
 			else
 			{
-				ph = w->UnstashParticle(_products[0]);
-				p->SetCharge(-_protoncharge);
-				p->SetMass(OldMass - ph->GetMass());
+				Nratio = comp1/((comp2+1.0)*(compph+1.0));
+				Korxn = exp(-_pKo);
 
+				ei = ffm->EvaluateHamiltonian(*p1, w->GetComposition(), w->GetVolume());
+				
+				p1->SetCharge(_c2);
+				p1->SetMass(_m2);
+				p1->SetSpeciesID(_i2);
+				
+				ef = ffm->EvaluateHamiltonian(*p1, w->GetComposition(), w->GetVolume());
+				
+				ph = w->UnstashParticle(_products[0]);
 				// Generate a random position and orientation for particle insertion.
 				const auto& H = w->GetHMatrix();
 				Vector3D pr{_rand.doub(), _rand.doub(), _rand.doub()};
 				Vector3D pos = H*pr;
 				ph->SetPosition(pos);
 
-				// Choose random axis, and generate random angle.
-				int axis = _rand.int32() % 3 + 1;
-				double deg = (4.0*_rand.doub() - 2.0)*M_PI;
-				Matrix3D R = GenRotationMatrix(axis, deg);
-
-				// Rotate particle and director.
-				ph->SetDirector(R*ph->GetDirector());
-				for(auto& child : *ph)
-				{
-					child->SetPosition(R*(child->GetPosition()-pos) + pos);
-					child->SetDirector(R*child->GetDirector());
-				}
-
 				// Insert particle.
 				w->AddParticle(ph);
-			}
-
-			auto id = ph->GetSpeciesID();
-			auto& comp = w->GetComposition();
-			auto V = pow(w->GetVolume(),RxnExtent);
-			auto lambda = w->GetWavelength(id);
-			auto lamda3 = pow(lambda*lambda*lambda,-1*RxnExtent);
-			double lamdaratio = pow(p->GetMass()/OldMass,3.0/2.0);
-			double Nratio=0;
-
-			if(RxnExtent==1)
-			{
-				Nratio = (comp[p->GetSpeciesID()]-_Ndeprotonated)/((_Ndeprotonated+1)*(comp[ph->GetSpeciesID()]+1));
-				Korxn = exp(-_pKo);
-			}
-			else
-			{
-				Nratio = double(_Ndeprotonated)*comp[ph->GetSpeciesID()]/(comp[p->GetSpeciesID()]-_Ndeprotonated + 1);
-				Korxn = exp(-_pKo);
+				ef += ffm->EvaluateHamiltonian(*ph, w->GetComposition(), w->GetVolume());
+				lamdaratio = pow(_c2/_c1,3.0/2.0);
 			}
 
 			++_performed;
-
-			auto ef = ffm->EvaluateHamiltonian(*w);
 
 			auto de = ef - ei;
 		
@@ -249,27 +309,36 @@ namespace SAPHRON
 			if(!(override == ForceAccept) && (pacc < _rand.doub() || override == ForceReject))
 			{
 				++_rejected;
-				p->SetCharge(OldCharge);
-				p->SetMass(OldMass);
-
-				if(RxnExtent == -1) {w->AddParticle(ph);}
-
-				else {w->StashParticle(ph);}
-
-			}
-			else
-			{
 				if(RxnExtent == -1) 
 				{
-					w->StashParticle(ph);
-					_Ndeprotonated-=1;
+					p2->SetCharge(_c2);
+					p2->SetMass(_m2);
+					p2->SetSpeciesID(_i2);
+					w->AddParticle(ph);
 				}
-				else
-					_Ndeprotonated+=1;
 
-				w->SetEnergy(ef.energy);
-				w->SetPressure(ef.pressure);
+				else 
+				{
+					p1->SetCharge(_c1);
+					p1->SetMass(_m1);
+					p1->SetSpeciesID(_i1);
+					w->RemoveParticle(ph);
+					w->StashParticle(ph);
+				}
+
 			}
+
+			else
+			{
+				if(RxnExtent == -1)
+				{
+					w->StashParticle(ph);
+				}
+
+				w->IncrementEnergy(de.energy);
+				w->IncrementPressure(de.pressure);
+			}
+			
 		}
 
 		virtual void Perform(World* world, 
@@ -277,140 +346,7 @@ namespace SAPHRON
 							 DOSOrderParameter* op, 
 							 const MoveOverride& override) override
 		{
-
-			// Draw random particle from random world.
-			auto* p = world->DrawRandomPrimitiveBySpecies(_reactants[0]);
-			Particle* ph =nullptr;
-
-			int RxnExtent = 1;
-			double rxndirection = _rand.doub();
-			double OldCharge = p->GetCharge();
-			double OldMass = p->GetMass();
-			double Korxn = 0;
-
-			//Determine reaction direction.
-			if(rxndirection>=0.5 && OldCharge >= 0) //Forward reaction
-			{
-				RxnExtent = 1;
-			}
-
-			else if(rxndirection<0.5 && OldCharge < 0)//Reverse reaction
-			{
-				RxnExtent = -1;
-				ph = world->DrawRandomPrimitiveBySpecies(_products[0]);
-
-				if(ph==nullptr)
-				{
-					std::cout<<"No products are present in system!"<<std::endl;
-					exit(-1);
-				}
-			}
-
-			else
-				return;
-
-			// Evaluate initial energy. 
-			auto ei = ffm->EvaluateHamiltonian(*world);
-			auto opi = op->EvaluateOrderParameter(*world);
-
-			if(RxnExtent == -1)
-			{
-				p->SetCharge(0.0);
-				p->SetMass(OldMass + ph->GetMass());
-				world->RemoveParticle(ph);
-			}
-
-			else
-			{
-				ph = world->UnstashParticle(_products[0]);
-				p->SetCharge(-_protoncharge);
-				p->SetMass(OldMass - ph->GetMass());
-
-				// Generate a random position and orientation for particle insertion.
-				const auto& H = world->GetHMatrix();
-				Vector3D pr{_rand.doub(), _rand.doub(), _rand.doub()};
-				Vector3D pos = H*pr;
-				ph->SetPosition(pos);
-
-				// Choose random axis, and generate random angle.
-				int axis = _rand.int32() % 3 + 1;
-				double deg = (4.0*_rand.doub() - 2.0)*M_PI;
-				Matrix3D R = GenRotationMatrix(axis, deg);
-
-				// Rotate particle and director.
-				ph->SetDirector(R*ph->GetDirector());
-				for(auto& child : *ph)
-				{
-					child->SetPosition(R*(child->GetPosition()-pos) + pos);
-					child->SetDirector(R*child->GetDirector());
-				}
-
-				// Insert particle.
-				world->AddParticle(ph);
-			}
-
-			auto id = ph->GetSpeciesID();
-			auto& comp = world->GetComposition();
-			auto V = pow(world->GetVolume(),RxnExtent);
-			auto lambda = world->GetWavelength(id);
-			auto lamda3 = pow(lambda*lambda*lambda,-1*RxnExtent);
-			double lamdaratio = pow(p->GetMass()/OldMass,3.0/2.0);
-			double Nratio=0;
-
-			if(RxnExtent==1)
-			{
-				Nratio = (double(comp[p->GetSpeciesID()]) - _Ndeprotonated)/((_Ndeprotonated+1)*(comp[ph->GetSpeciesID()]+1));
-				Korxn = exp(-_pKo);
-			}
-			else
-			{
-				Nratio = (double(_Ndeprotonated)*comp[ph->GetSpeciesID()])/(comp[p->GetSpeciesID()]-_Ndeprotonated + 1);
-				Korxn = exp(-_pKo);
-			}
-
-			++_performed;
-
-			auto ef = ffm->EvaluateHamiltonian(*world);
-
-			// Update energies and pressures.
-			world->SetEnergy(ef.energy);
-			world->SetPressure(ef.pressure);
-
-			auto opf = op->EvaluateOrderParameter(*world);
-			
-			// Acceptance probability.
-			double pacc = op->AcceptanceProbability(ei.energy, ef.energy, opi, opf, *world);
-			if(_prefac)
-			{
-				auto arg = Nratio*V*lamda3*lamdaratio*Korxn;
-				pacc *= arg;
-			}
-			pacc = pacc > 1.0 ? 1.0 : pacc;
-
-			// Reject or accept move.
-			if(!(override == ForceAccept) && (pacc < _rand.doub() || override == ForceReject))
-			{
-				++_rejected;
-				p->SetCharge(OldCharge);
-				p->SetMass(OldMass);
-				world->SetEnergy(ei.energy);
-				world->SetPressure(ei.pressure);
-
-				if(RxnExtent == -1) {world->AddParticle(ph);}
-
-				else {world->StashParticle(ph);}
-
-			}
-			else
-			{
-				if(RxnExtent == -1) 
-				{
-					world->StashParticle(ph);
-					_Ndeprotonated-=1;
-				}
-				else
-					_Ndeprotonated+=1;
-			}
+			return;
 		}
 
 		// Turns on or off the acceptance rule prefactor for DOS order parameter.
@@ -436,11 +372,11 @@ namespace SAPHRON
 			json["stash_count"] = _scount;
 			json["op_prefactor"] = _prefac;
 
-			for(auto& s : _reactants)
-				json["reactants"].append(s);
-
+			auto& slist = Particle::GetSpeciesList();
+			for(auto& s : _swap)
+				json["swap"].append(slist[s]);
 			for(auto& s : _products)
-				json["products"].append(s);
+				json["products"].append(slist[s]);
 		}
 
 		virtual std::string GetName() const override { return "AcidReaction"; }
