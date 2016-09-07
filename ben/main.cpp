924aa1b1a52880e5925818064e91396c06127613
Ben S <bsikora906@gmail.com>
2016-09-07 11:31:03 -0400

    Update to main.cpp file for Vik

--- ben/main.cpp
+++ ben/main.cpp
@@ -9,7 +9,7 @@
 #include <omp.h>
 #include <time.h>
 #include "../src/ForceFields/ForceFieldManager.h"
-#include "../src/ForceFields/LennardJonesFF.h"
+#include "../src/ForceFields/LennardJonesTSFF.h"
 #include "../src/ForceFields/FENEFF.h"
 #include "../src/Moves/MoveManager.h"
 #include "../src/Particles/Particle.h"
@@ -93,81 +93,90 @@ int main(int narg, char **arg)
 
   if (lammps == 1) // if lammps has right number of processors
   {
-    string eachLine;
-    ifstream infile;
-    infile.open ("in.lj");
-    
-    while(!infile.eof()) // get all lines
-    {
-      getline(infile,eachLine); // Saves the line in eachLine.
-      lmp->input->one((char*)eachLine.c_str());
-    }
-    
-    infile.close();
+    for(int i=0; i<20;i++)
+      {// THIS FOR LOOP SIMULATES RUN (FOR E.G. RUN 100 IN LAMMPS WOULD CORRESPOND TO i<100 HERE), CAN LATER BE PUT AS ARGUMENT
+        lmp->input->one("run 1");
+        int natoms = static_cast<int> (lmp->atom->natoms); // get total numb atoms in simulation
+        double *x = new double[3*natoms]; // single array 3* times cause x,y and z
+        lammps_gather_atoms(lmp,"x",1,3,x); //  gathers all coords arranged by id , for type double, 3 elements per id and fill x
+        Rand _rand(time(NULL));   // seeding random number generator with time (every time you run it's gonna be diff)
+        // can replace with seed value
+        double* lammps_energy = ((double*) lammps_extract_variable(lmp,"tot","0")); //THIS WORKS
+        
 
-    int natoms = static_cast<int> (lmp->atom->natoms); // get total numb atoms in simulation
-    double *x = new double[3*natoms]; // single array 3* times cause x,y and z
-    lammps_gather_atoms(lmp,"x",1,3,x); //  gathers all coords , for type double, 3 elements per id and fill x
+        cout << "*******************************************"<<endl;
+        cout <<"LAMMPS TOTAL ENERGY (ke+pe)"<<*lammps_energy<<endl; 
+        cout<<"TOTAL NO. OF ATOMS FROM LAMMPS "<<natoms<<endl;
 
-    Rand _rand(time(NULL));   // seeding random number generator with time (every time you run it's gonna be diff)
-    // can replace with seed value
-    lmp->input->one("run 1");
-    //*******************************************************************************
-    // set rcut
-    double rcut = 12.0;   // ???
+        //*******************************************************************************
+        // set rcut WORLD
+        double rcut = 3.0;   // ???
 
-    //Set simulation
-    World world(100, 100, 100, rcut, 46732); //create a world
+        //Set simulation
+        World world(10.38498, 10.38498, 10.38498, rcut, 46732); //create a world simualar to lammps
 
-    SAPHRON::Particle poly("Polymer"); //Create the polymer 
-    ParticleList Monomers;
+        SAPHRON::Particle poly("Polymer"); //Create the polymer 
+        ParticleList Monomers;
 
-    for(int i=0; i<natoms*3;i=i+3){
-      Monomers.push_back(new Particle({x[i],x[i+1],x[i+2]},{0.0,0.0,0.0}, "Monomer")); //Particle constructor
-    }
+        for(int i=0; i<natoms*3;i=i+3){
+          Monomers.push_back(new Particle({x[i],x[i+1],x[i+2]},{0.0,0.0,0.0}, "Monomer")); //Particle constructor
+        }
+  
+        for(int i=1; i<natoms-1;i++){
+          Monomers[i]->AddBondedNeighbor(Monomers[i+1]); // AddBondedNeighbor instead of AddNeighbor
+          Monomers[i]->AddBondedNeighbor(Monomers[i-1]);
+        }
+        Monomers[0]->AddBondedNeighbor(Monomers[1]);
+        Monomers[natoms-1]->AddBondedNeighbor(Monomers[natoms-2]);
+  
+        for(auto& c : Monomers)
+        {
+          poly.AddChild(c);
+        }
+  
+  
+        world.AddParticle(&poly);   // instead of PackWorld, packWorld was filling up the box
+        world.UpdateNeighborList();
+        //world.PackWorld({&poly}, {1.0}, 1, 0.00357); // polymer, number fractions, n = total number, density=resize box
+        // ABOVE VALUES IN PACKWORLD CORRESPOND SYSTEM IN LAMMPS
 
-    for(int i=1; i<natoms-1;i++){
-      Monomers[i]->AddNeighbor(Monomers[i+1]);
-      Monomers[i]->AddNeighbor(Monomers[i-1]);
-    }
-    Monomers[0]->AddNeighbor(Monomers[1]);
-    Monomers[natoms-1]->AddNeighbor(Monomers[natoms-2]);
+        ForceFieldManager ffm;
 
-    for(auto& c : Monomers)
-    {
-      poly.AddChild(c);
-    }
+        LennardJonesTSFF lj(0.8, 1.0, {1.122}); // eps, sigma, cutoff. SIMILAR TO LAMMPS
+        FENEFF fene(1.0, 1.0, 30.0, 2.0); // eps, sigma, kspring, rmax, will get value from lammps. SIMILAR TO LAMMPS
 
-    world.PackWorld({&poly}, {1.0}, 1, 0.0001); // polymer, number fractions, n = total number, density=resize box
+        //SAPHTRON forcefield
+        ffm.AddNonBondedForceField("Monomer", "Monomer", lj);
+        ffm.AddBondedForceField("Monomer", "Monomer", fene);
 
-    ForceFieldManager ffm;
+        //Gather the energy from saphron
+        auto H1 = ffm.EvaluateEnergy(world);
 
-    LennardJonesFF lj(10.0, 1.0, {4.0}); // eps, sigma, cutoff
-    FENEFF fene(10.0, 1.0, 4.0, 1.0); // eps, sigma, kspring, rmax, will get value from lammps
+        cout<<"TOTAL ENERGY OF TEH WORLD FROM SAPHRON "<<H1.energy.total()<<endl;
+  
+        // TESTING COORDS saphron gets
+        for(auto& p : world)
+        {
+          for(auto& c1 : *p) {
+            Position pos = c1->GetPosition();
+            std::cout << pos[0] << " " << pos[1] << " " << pos[2] << std::endl;
+          }
+        }
 
-    //SAPHTRON forcefield
-    ffm.AddNonBondedForceField("Monomer", "Monomer", lj);
-    ffm.AddBondedForceField("Monomer", "Monomer", fene);
+    //********************************************************************//
 
-    //Gather the energy from saphron
-    auto H1 = ffm.EvaluateEnergy(world);
+        // PRINTING OUT VALUES OF x
+        for(int i = 0; i < 12; i++) {
+          cout << *(x + i) << endl;
+        }
 
-//********************************************************************//
-
-    // End Reference & run lammps
-    delete [] x;
-
-    MPI_Barrier(MPI_COMM_WORLD);
-    
-    // I dont know if this works but you can try it Vik
-    double* lammps_energy = ((double*) lammps_extract_variable(lmp,"t","all"));
-      
-    MPI_Barrier(MPI_COMM_WORLD);
-
+        delete [] x;
+        MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
   }
+}
 
   if (lammps == 1) delete lmp;
-
   // close down MPI
   MPI_Finalize();
 }
